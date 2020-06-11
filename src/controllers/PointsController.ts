1dd3a9474f1ec4a1d2b359e9e93ea12310f42354
Diego Simoes <simoes.sdgo@gmail.com>
2020-06-11 05:17:07 -0300

    feat: added route to find the points from the city, uf and the items

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -4,6 +4,25 @@ import knex from '../database/connection';
 import Const from '../constants';
 
 class PointController {
+    
+    async index(request: Request, response: Response) {
+        const { city, uf, items } = request.query;
+
+        const parcedItems = String(items)
+            .split(',')
+            .map( item => Number(item.trim()));
+
+        const pointsFound = await knex(`${Const.TABLE_POINTS} as p`)
+            .join(`${Const.TABLE_POINT_ITEMS} as pi`, 'pi.point_id', '=', 'p.id')
+            .whereIn('pi.item_id', parcedItems)
+            .where('p.city', String(city))
+            .where('p.uf', String(uf))
+            .select('name')
+            .distinct()
+            .select('p.*');
+
+        return response.json(pointsFound);
+    }
 
     async show(request: Request, response: Response) {
         const { id } = request.params;
