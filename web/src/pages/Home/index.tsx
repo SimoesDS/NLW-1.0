8f1fe3ec85743a569fff1d8de0120abe645d3095
Diego Simões <simoes.sdgo@gmail.com>
2020-06-20 19:01:29 -0300

    feat: created HTML form

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './Home.css';
 
 import { FiLogIn } from 'react-icons/fi';
-
 import logo from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -17,12 +17,12 @@ const Home = () => {
                     <h1>Seu marketplace de coleta de resíduos.</h1>
                     <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
 
-                    <a href="/cadastro">
+                    <Link to="/create-point">
                         <span>
                             <FiLogIn />
                         </span>
                         <strong>Cadastre um ponto de coleta</strong>
-                    </a>
+                    </Link>
                 </main>
             </div>
         </div>
